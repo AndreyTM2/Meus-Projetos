377de3736bd0745dacd4e837ae4e7d80bf4b95d5
AndreyTM2 <106534302+AndreyTM2@users.noreply.github.com>
2023-06-06 12:20:06 -0300

    Update javascript.js

--- Programação/Aula ??/Scripts/javascript.js
+++ Programação/Aula ??/Scripts/javascript.js
@@ -27,3 +27,23 @@ function load () {
 
     lista.innerHTML = listaProdutos;
 }
+
+
+
+/*/
+async function load () { 
+    const lista = document.getElementById("4165");
+    const link = `https://pokeapi.co/api/v2/pokemon?limit=${[20, 40, 65, 591, 327, 1000, 5000, 6000, 100000][Math.floor(Math.random() * 5)]}`
+    const _fetch = await fetch(link).then(async (_) => _.json());
+    const pokemon = await Promise.all(_fetch.results.map(async ({ name, url }) => {
+        const pokemonReal = await fetch(url).then(async (i) => i.json());
+        const at = (pokemonReal.abilities.map((i) => i.ability.name)).join(", ");
+        const nome = name[0].toUpperCase() + name.slice(1);
+        const preço = (((Math.random() * pokemonReal.base_experience)) * Math.random() * 45).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
+        return `<li class="product-box"><img src="${pokemonReal.sprites.front_default}" alt="${at}"><div class="product-name">${nome}</div><div class="product-price">${preço}</div></li>`
+    }))
+
+    
+    lista.innerHTML = pokemon    
+}
+/*/
