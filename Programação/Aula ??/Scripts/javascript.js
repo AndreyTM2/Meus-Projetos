60c652277b18c260c35ef1904b6facc5a8b7d28d
AndreyTM2 <106534302+AndreyTM2@users.noreply.github.com>
2023-06-06 12:21:54 -0300

    Update javascript.js

--- Programação/Aula ??/Scripts/javascript.js
+++ Programação/Aula ??/Scripts/javascript.js
@@ -37,10 +37,11 @@ async function load () {
     const _fetch = await fetch(link).then(async (_) => _.json());
     const pokemon = await Promise.all(_fetch.results.map(async ({ name, url }) => {
         const pokemonReal = await fetch(url).then(async (i) => i.json());
-        const at = (pokemonReal.abilities.map((i) => i.ability.name)).join(", ");
+        const at = (pokemonReal.abilities.map((i) => i.ability.name[0].toUpperCase() + i.ability.name.slice(1))).join(" ");
         const nome = name[0].toUpperCase() + name.slice(1);
+        
         const preço = (((Math.random() * pokemonReal.base_experience)) * Math.random() * 45).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
-        return `<li class="product-box"><img src="${pokemonReal.sprites.front_default}" alt="${at}"><div class="product-name">${nome}</div><div class="product-price">${preço}</div></li>`
+        return `<li class="product-box"><img src="${pokemonReal.sprites.front_default}" alt="${at}"><div class="product-name">${nome}<p>${at}</div><div class="product-price">${preço}</div></li>`
     }))
 
     
